484d8706b949474b8adc67d4afd32abf701624cf
aoiacai <102672343+aoiacai@users.noreply.github.com>
2024-09-03 23:35:45 +0900

    feat: helloworldを追加

---
+++ app/components/Helloworld.tsx
@@ -0,0 +1,10 @@
+function Helloworld() {
+	return (
+		<>
+			<div className="text-center">
+				<h1 className="text-7xl font-bold py-40">Hello World</h1>
+			</div>
+		</>
+	);
+}
+export default Helloworld;
