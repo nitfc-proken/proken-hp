36e5fb21fd74d7d0dfc5007e472bb46d6c0fecf8
void2610 <izumisan2610@gmail.com>
2024-09-03 22:58:26 +0900

    ページ遷移を実装

---
+++ app/work/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const WorkPage = () => {
+ return (
+  <>
+   <Header />
+
+   <Footer />
+  </>
+ );
+};
+
+export default WorkPage;
