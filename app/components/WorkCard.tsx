acfb9c09f5896dd9493c30155a77be2dbbd6ad87
void2610 <izumisan2610@gmail.com>
2024-09-08 01:22:17 +0900

    スーパーマッスルレスポンスデザイン

--- app/components/WorkCard.tsx
+++ app/components/WorkCard.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
-import { Box, Image, Button, Avatar } from "@chakra-ui/react";
+import { Box, Image, Button, Avatar, Text, Flex } from "@chakra-ui/react";
+import { useBreakpointValue } from "@chakra-ui/media-query";
+import { mediaQuery, useMediaQuery } from "../hooks/useMediaQuery";
 import styles from "../styles/WorkCard.module.css";
 
 interface WorkCardProps {
@@ -25,6 +27,9 @@ const WorkCard: React.FC<WorkCardProps> = ({ title, creator, description, link, 
   window.open(link, "_blank");
  };
 
+ // スマホかどうか
+ const isSp = useMediaQuery(mediaQuery.sp);
+
  return (
   <div
    className={`${styles.card} ${isFlipped ? styles.isFlipped : ""}`}
@@ -42,26 +47,26 @@ const WorkCard: React.FC<WorkCardProps> = ({ title, creator, description, link, 
     </Box>
    </div>
    {/* 裏面 */}
-   <div className={`${styles.cardface} ${styles.cardfaceBack}`}>
+   <div className={`${styles.cardface} ${styles.cardfaceBack}`} style={{ width: "100%", height: "100%" }}>
     <Image src={image} alt={title} width="100%" height="100%" objectFit="cover" borderRadius="1vw" style={{ filter: "blur(20px)" }} overflow="hidden" />
-    <Box p={4} textAlign="center" position="absolute" top="0" left="0" width="100%" height="100%" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-     <Box color="white" fontWeight="semibold" letterSpacing="wide" fontSize="xs" textTransform="uppercase">
-      {date}
-     </Box>
-     <Box mt="1" fontWeight="semibold" lineHeight="tight" isTruncated color="white">
+    <Text color="white" fontWeight="normal" letterSpacing="wide" fontSize="5xs" textTransform="uppercase" position="absolute" top="0" right="0" mr={2} mt={0.5}>
+     {date}
+    </Text>
+    <Flex p={4} textAlign="center" position="absolute" top="0" left="0" width="100%" height="100%" flexDirection="column" justifyContent="center" alignItems="center">
+     <Text fontWeight="bold" color="white" textAlign="center" position="absolute" top="15%" fontSize={isSp ? "200%" : "140%"} mb={isSp ? "1%" : "0%"}>
       {title}
-     </Box>
-     <Box color="white" fontSize="medium" display="flex" alignItems="center">
-      <Avatar src={`/member/${creator}.png`} size="sm" name={creator} mr={2} />
-      {creator}
-     </Box>
-     <Box mt="2" color="white" fontSize="sm" textAlign="left">
+     </Text>
+     <Flex color="white" fontSize="medium" alignItems="center" width="100%" height="10%" justifyContent="center" mt={isSp ? "0%" : "5%"}>
+      <Avatar src={`/member/${creator}.png`} name={creator + "_avatar"} mr={2} size={useBreakpointValue({ base: "2xs", md: "xs" })} />
+      <Text fontSize={isSp ? "10%" : "90%"}>{creator}</Text>
+     </Flex>
+     <Text color="white" textAlign="center" fontSize="60%" position="absolute" top="63%" w={isSp ? "80%" : "60%"}>
       {description}
-     </Box>
-     <Button mt={4} colorScheme="teal" onClick={handleLinkClick}>
+     </Text>
+     <Button colorScheme="teal" onClick={handleLinkClick} fontSize="80%" position="absolute" bottom="5%" w={isSp ? "20%" : "25%"} h={isSp ? "15%" : "17%"}>
       Visit Link
      </Button>
-    </Box>
+    </Flex>
    </div>
   </div>
  );
