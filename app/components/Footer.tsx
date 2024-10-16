ec85a706731cb0db40efc9fd47362bb7aa304103
void2610 <izumisan2610@gmail.com>
2024-10-16 16:32:17 +0900

    フッター実装

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,21 @@
-function Footer(){
-    return (
-        <footer>
-            <div>Footer</div>
-        </footer>
-    );
-}export default Footer;
+import { Image } from "@chakra-ui/react";
+
+function Footer() {
+ return (
+  <footer style={{ backgroundColor: "gray", height: "20vw", display: "flex", justifyContent: "space-between", alignItems: "center", padding: "4vw", maxHeight: "5%" }}>
+   <div style={{ display: "flex", flexDirection: "column", justifyContent: "right", gap: "4vw" }}>
+    <p>プログラミング研究会</p>
+    <p>hogehoge by 部長</p>
+   </div>
+   <div style={{ display: "flex", justifyContent: "right", gap: "4vw" }}>
+    <a href="https://discord.com/invite/your-discord-invite-link" target="_blank" rel="noopener noreferrer">
+     <Image src="svgs/discord-icon-svgrepo-com.svg" alt="discord-logo" boxSize="10vw" />
+    </a>
+    <a href="https://github.com/nitfc-proken" target="_blank" rel="noopener noreferrer">
+     <Image src="svgs/github-mark.svg" alt="github-logo" boxSize="10vw" />
+    </a>
+   </div>
+  </footer>
+ );
+}
+export default Footer;
