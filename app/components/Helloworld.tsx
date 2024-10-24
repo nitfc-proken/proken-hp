b9026961422271dd7be47475d23f0cfef160cd39
aoiacai <102672343+aoiacai@users.noreply.github.com>
2024-10-24 21:08:54 +0900

    helloworldをカラフルかつズームする動きに変更

--- app/components/Helloworld.tsx
+++ app/components/Helloworld.tsx
@@ -1,10 +1,12 @@
+import React from "react";
+import "./helloworld.css"; // スタイルをインポート
+
 function Helloworld() {
 	return (
-		<>
-			<div className="text-center">
-				<h1 className="text-7xl font-bold py-40">Hello World</h1>
-			</div>
-		</>
+		<div className="text-center p-10">
+			<h1 className="hello-world">Hello World</h1>
+		</div>
 	);
 }
+
 export default Helloworld;
