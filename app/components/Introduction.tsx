c11c48f2ffa9ecce982c740c0a45a13332696a3f
aoiacai <102672343+aoiacai@users.noreply.github.com>
2024-10-24 21:39:49 +0900

    変な場所で改行される問題を修正

--- app/components/Introduction.tsx
+++ app/components/Introduction.tsx
@@ -5,7 +5,7 @@ function Introduction() {
 				<h1 className="text-3xl font-bold py-3">Introduction</h1>
 			</div>
 			<div className="flex justify-center items-center">
-				<p className="text-2xl font-bold py-3 max-w-screen-md text-center">
+				<p className="text-2xl font-bold text-center max-w-[50rem]">
 					福井高専プログラミング研究会は、ゲーム制作や競技プログラミングなどプログラミングに関することを幅広く行う同好会です。
 				</p>
 			</div>
