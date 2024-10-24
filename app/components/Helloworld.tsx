c1287b2077912c6519f25913d2b86d47df4201d2
Tomoya Takahashi <102672343+aoiacai@users.noreply.github.com>
2024-10-24 21:43:32 +0900

    Editing main.tsx (#29)
    Co-authored-by: Shuya Izumi <52399972+void2610@users.noreply.github.com>

---
+++ app/components/Helloworld.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./helloworld.css"; // スタイルをインポート
+
+function Helloworld() {
+	return (
+		<div className="text-center p-10">
+			<h1 className="hello-world">Hello World</h1>
+		</div>
+	);
+}
+
+export default Helloworld;
---
+++ app/components/Introduction.tsx
@@ -0,0 +1,15 @@
+function Introduction() {
+	return (
+		<div className="p-10">
+			<div className="flex justify-center items-center">
+				<h1 className="text-3xl font-bold py-3">Introduction</h1>
+			</div>
+			<div className="flex justify-center items-center">
+				<p className="text-2xl font-bold text-center max-w-[50rem]">
+					福井高専プログラミング研究会は、ゲーム制作や競技プログラミングなどプログラミングに関することを幅広く行う同好会です。
+				</p>
+			</div>
+		</div>
+	);
+}
+export default Introduction;
