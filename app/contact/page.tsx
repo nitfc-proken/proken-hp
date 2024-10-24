c00f0abbf6b23d1acf36fa69888379dfa0bc51c8
cassini117 <nhtg2086@gmail.com>
2024-10-25 00:27:05 +0900

    さらに変なの追加

---
+++ app/contact/page.tsx
@@ -0,0 +1,19 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex-grow flex justify-center items-center p-10">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-3xl font-bold py-3">Contact</h1>
+          <p className="text-2xl font-bold text-center max-w-[50rem]">
+            そんなものはない
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
