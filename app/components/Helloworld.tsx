94789a05a2c93c262528a4021e72ca2766f45f53
cassini117 <nhtg2086@gmail.com>
2024-10-25 01:09:56 +0900

    Hello

--- app/components/Helloworld.tsx
+++ app/components/Helloworld.tsx
@@ -7,25 +7,27 @@ function Helloworld() {
   return (
     <div className="text-center h-screen flex flex-col justify-center items-center px-5 sm:px-10 lg:px-20">
       <div className="text-center">
-        <motion.h1
-          className="hello-world text-7xl font-bold py-5 sm:text-5xl md:text-6xl lg:text-7xl"
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.3, ease: "easeOut" }}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.95 }}
-          drag
-          dragConstraints={{
-            top: -10,
-            left: -10,
-            right: 10,
-            bottom: 10,
-          }}
-        >
-          Hello
-          <br className="block sm:hidden" />
-          World
-        </motion.h1>
+        <div className="flex justify-center items-center">
+          <motion.h1
+            className="hello-world mx-auto"
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
+            drag
+            dragConstraints={{
+              top: -5,
+              left: -5,
+              right: 5,
+              bottom: 5,
+            }}
+          >
+            Hello
+            <br className="block sm:hidden" />
+            World
+          </motion.h1>
+        </div>
         <motion.h2
           className="text-3xl font-bold py-5 sm:text-2xl md:text-3xl lg:text-4xl"
           initial={{ opacity: 0, y: 50 }}
