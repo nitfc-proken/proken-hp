8dd39437d747961de1d56a506e302f9b3932ce1e
void2610 <izumisan2610@gmail.com>
2024-10-24 16:04:31 +0900

    サイズを変数に

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,20 +1,22 @@
 import { Image } from "@chakra-ui/react";
 
 function Footer() {
+ const imageSize = "4vw";
+ const textSize = "2vw";
  return (
   <>
    <div style={{ height: "20vw" }}></div>
    <footer style={{ backgroundColor: "#7AB8BF", height: "auto", display: "flex", justifyContent: "space-between", alignItems: "center", padding: "4vw", maxHeight: "80px" }}>
     <div style={{ display: "flex", flexDirection: "column", justifyContent: "right", gap: "50%" }}>
-     <p style={{ fontSize: "90%" }}>プログラミング研究会</p>
-     <p style={{ fontSize: "90%" }}>部員募集中!</p>
+     <p style={{ fontSize: textSize }}>プログラミング研究会</p>
+     <p style={{ fontSize: textSize }}>部員募集中!</p>
     </div>
-    <div style={{ display: "flex", justifyContent: "right", alignItems: "center", gap: "20px" }}>
+    <div style={{ display: "flex", justifyContent: "right", alignItems: "center", gap: "2.5vw" }}>
      <a href="https://discord.com/invite/your-discord-invite-link" target="_blank" rel="noopener noreferrer">
-      <Image src="svgs/discord-icon-svgrepo-com.svg" alt="discord-logo" width="50px" height="50px" /> {/* サイズを統一 */}
+      <Image src="svgs/discord-icon-svgrepo-com.svg" alt="discord-logo" width={imageSize} height={imageSize} />
      </a>
      <a href="https://github.com/nitfc-proken" target="_blank" rel="noopener noreferrer">
-      <Image src="svgs/github-mark.svg" alt="github-logo" width="50px" height="50px" /> {/* サイズを統一 */}
+      <Image src="svgs/github-mark.svg" alt="github-logo" width={imageSize} height={imageSize} />
      </a>
     </div>
    </footer>
