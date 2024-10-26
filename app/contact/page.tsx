8ac8e2f3447dd5ae4bef64060eaa05e4351c223a
LITl-l <52400018+LITl-l@users.noreply.github.com>
2024-10-26 14:30:29 +0900

    ➕  chore: biomeの導入 & format (#32)
    Co-authored-by: void2610 <izumisan2610@gmail.com>

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,27 +1,30 @@
-import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Header from "../components/Header";
 import { Link } from "@chakra-ui/react";
 
 export default function Home() {
- return (
-  <div className="flex flex-col min-h-screen">
-   <Header />
-   <div className="flex-grow flex justify-center items-center p-10">
-    <div className="flex flex-col justify-center items-center">
-     <h1 className="text-3xl font-bold py-3">Contact</h1>
-     <p className="text-2xl font-bold text-center max-w-[50rem]">
-      主にDiscordを用いてコミュニケーションを取っています。
-      <br /> 入部や見学などはいつでも受け付けていますので、
-      <br />
-      気軽に
-      <Link color="teal.500" href="https://discord.com/invite/your-server-id">
-       Discordサーバー
-      </Link>
-      へ参加してください！
-     </p>
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex-grow flex justify-center items-center p-10">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-3xl font-bold py-3">Contact</h1>
+          <p className="text-2xl font-bold text-center max-w-[50rem]">
+            主にDiscordを用いてコミュニケーションを取っています。
+            <br /> 入部や見学などはいつでも受け付けていますので、
+            <br />
+            気軽に
+            <Link
+              color="teal.500"
+              href="https://discord.com/invite/your-server-id"
+            >
+              Discordサーバー
+            </Link>
+            へ参加してください！
+          </p>
+        </div>
+      </div>
+      <Footer />
     </div>
-   </div>
-   <Footer />
-  </div>
- );
+  );
 }
